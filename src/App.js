be5cd6e5dddb7319900159660f2c797af3488d1f
Mwau stephen <stephenmwau41@gmail.com>
2022-07-15 16:58:08 +0800

    added a post request for my form data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React, { useCallback, useEffect } from "react";
+
 import BasicForm from "./Components/BasicForm";
 
-function App() {
+const App = () => {
+  const formHandler = (name, email, password) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://post-form-data-f16ec-default-rtdb.firebaseio.com/form.json",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+    };
+    fetchData();
+  };
+
+  // const formHandler = (name, email, password) => {
+  //   console.log(name);
+  //   console.log(email);
+  //   console.log(password);
+  // };
+
+  // useEffect(() => {}, []);
+
   return (
     <div className="app">
-      <BasicForm />
+      <BasicForm sendFormData={formHandler} />
     </div>
   );
-}
+};
 
 export default App;
