12e6d8c8d114a7bef904e6745a15e17df2a6bcae
Mwau stephen <stephenmwau41@gmail.com>
2022-07-17 14:01:38 +0800

    imported the custome hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useCallback, useEffect } from "react";
-
+import postRequestHook from "./Components/Hooks/use-https-request";
 import BasicForm from "./Components/BasicForm";
 
 const App = () => {
   const formHandler = (name, email, password) => {
+    // const sendRequest = async () => {
+    //   postRequestHook({
+    //     url: "https://post-form-data-f16ec-default-rtdb.firebaseio.com/form.json",
+    //     method: "POST",
+    //     body: { name: name, email: email, passowrd: password },
+    //     Headers: { "Content-Type": "application/json" },
+    //   });
+    // };
+    // sendRequest();
     const fetchData = async () => {
       const response = await fetch(
         "https://post-form-data-f16ec-default-rtdb.firebaseio.com/form.json",
@@ -20,6 +29,15 @@ const App = () => {
         }
       );
       console.log(response);
+
+      const data = await response.json();
+      const transformedData = {
+        name: data.name,
+        email: data.email,
+        password: data.password,
+      };
+
+      console.log(transformedData);
     };
     fetchData();
   };
