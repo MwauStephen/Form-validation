9e58b4fc8bdc289e1d180d9720c0b48b06b98e6c
Mwau stephen <stephenmwau41@gmail.com>
2022-07-17 16:22:23 +0800

    rolled back to the initial http request state

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React, { useCallback, useEffect } from "react";
 import postRequestHook from "./Components/Hooks/use-https-request";
-import MessageModal from "./Components/Modal/MessageModal";
+
 import BasicForm from "./Components/BasicForm";
 
 const App = () => {
   const formHandler = (name, email, password) => {
-    // const sendRequest = async () => {
-    //   postRequestHook({
-    //     url: "https://post-form-data-f16ec-default-rtdb.firebaseio.com/form.json",
-    //     method: "POST",
-    //     body: { name: name, email: email, passowrd: password },
-    //     Headers: { "Content-Type": "application/json" },
-    //   });
-    // };
-    // sendRequest();
-    const fetchData = async () => {
-      const response = await fetch(
-        "https://post-form-data-f16ec-default-rtdb.firebaseio.com/form.json",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            name: name,
-            email: email,
-            password: password,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      console.log(response);
+    const sendRequest = async () => {
+      postRequestHook({
+        url: "https://post-form-data-f16ec-default-rtdb.firebaseio.com/form.json",
+        method: "POST",
+        body: { name: name, email: email, passowrd: password },
+        Headers: { "Content-Type": "application/json" },
+      });
     };
-    fetchData();
+    sendRequest();
+    // const fetchData = async () => {
+    //   const response = await fetch(
+    //     "https://post-form-data-f16ec-default-rtdb.firebaseio.com/form.json",
+    //     {
+    //       method: "POST",
+    //       body: JSON.stringify({
+    //         name: name,
+    //         email: email,
+    //         password: password,
+    //       }),
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     }
+    //   );
+    //   console.log(response);
+    // };
+    // fetchData();
   };
 
   // const formHandler = (name, email, password) => {
@@ -45,7 +45,6 @@ const App = () => {
   return (
     <div className="app">
       <BasicForm sendFormData={formHandler} />
-      <MessageModal />
     </div>
   );
 };
