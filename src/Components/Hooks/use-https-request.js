1188b933e47dadc941f0eee497fc3e3b9f8138e9
Mwau stephen <stephenmwau41@gmail.com>
2022-07-17 14:00:29 +0800

    added a custom http hook

--- src/Components/Hooks/use-https-request.js
+++ src/Components/Hooks/use-https-request.js
@@ -1,3 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+const postRequestHook = (requestConfig) => {
+  const postData = async () => {
+    const response = await fetch(requestConfig.url, {
+      method: requestConfig.method ? requestConfig.method : "GET",
+      headers: requestConfig.headers ? requestConfig.headers : {},
+      body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+    });
 
+    console.log(response);
+
+    // const data = await response.json();
+
+    // console.log(data);
+  };
+  postData();
+  return;
+};
+
+export default postRequestHook;
